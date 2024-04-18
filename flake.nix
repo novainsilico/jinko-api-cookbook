1255846e708b23ade580beb31a66838648c44a9e
Jean Ponchon <jean.ponchon@novadiscovery.com>
2024-04-18 16:57:52 +0200

    Initialize

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "Jinko API Documentation";
+  nixConfig.extra-substituters = [
+    "https://tweag-jupyter.cachix.org"
+  ];
+  nixConfig.extra-trusted-public-keys = [
+    "tweag-jupyter.cachix.org-1:UtNH4Zs6hVUFpFBTLaA4ejYavPo5EFFqgd7G7FxGW9g="
+  ];
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.jupyenv.url = "github:tweag/jupyenv";
+  outputs =
+    { self
+    , flake-utils
+    , nixpkgs
+    , jupyenv
+    , ...
+    } @ inputs:
+    flake-utils.lib.eachSystem
+      [
+        flake-utils.lib.system.x86_64-linux
+      ]
+      (
+        system:
+        let
+          inherit (jupyenv.lib.${system}) mkJupyterlabNew;
+          jupyterlab = mkJupyterlabNew ({ ... }: {
+            nixpkgs = inputs.nixpkgs;
+            imports = [
+              ({ pkgs, ... }: {
+                kernel.python.scientifi = {
+                  enable = true;
+                  extraPackages = ps: [
+                    ps.numpy
+                    ps.scipy
+                    ps.matplotlib
+                    ps.flask
+                  ];
+                };
+              })
+            ];
+          });
+        in
+        {
+          packages = {
+            inherit jupyterlab;
+            git-lfs = nixpkgs.legacyPackages.${system}.git-lfs; # Add git-lfs here
+          };
+          packages.default = jupyterlab;
+          apps.default.program = "${jupyterlab}/bin/jupyter-lab";
+          apps.default.type = "app";
+          # kernel.python.aiml.enable = true;
+        }
+      );
+}
