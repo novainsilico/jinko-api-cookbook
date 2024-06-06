56c8d3e7489ac5f4689de2d8e4d4626afef8c194
Jean Ponchon <jean.ponchon@novadiscovery.com>
2024-06-06 15:59:24 +0200

    chore: simplify nix configuration with only one source of truth for jupyter packages (poetry)

--- flake.nix
+++ flake.nix
@@ -1,19 +1,11 @@
 {
-  description = "Jinko API Documentation";
-  nixConfig.extra-substituters = [
-    "https://tweag-jupyter.cachix.org"
-  ];
-  nixConfig.extra-trusted-public-keys = [
-    "tweag-jupyter.cachix.org-1:UtNH4Zs6hVUFpFBTLaA4ejYavPo5EFFqgd7G7FxGW9g="
-  ];
+  description = "Jinko Cookbook Documentation";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.jupyenv.url = "github:tweag/jupyenv";
   outputs =
     { self
     , flake-utils
     , nixpkgs
-    , jupyenv
     , ...
     } @ inputs:
     flake-utils.lib.eachSystem
@@ -23,57 +15,52 @@
       (
         system:
         let
-          inherit (jupyenv.lib.${system}) mkJupyterlabNew;
           pkgs = import nixpkgs { inherit system; };
-          jupyterlab = mkJupyterlabNew ({ ... }: {
-            nixpkgs = inputs.nixpkgs;
-            imports = [
-              ({ pkgs, ... }: {
-                kernel.python.jinko = {
-                  enable = true;
-                  extraPackages = ps: [
-                    ps.numpy
-                    ps.scipy
-                    ps.matplotlib
-                    ps.requests
-                  ];
-                };
-              })
-            ];
-          });
+          shellBuildInputs = [
+            # Poetry is the default package manager for the cookbook project
+            pkgs.poetry
+
+            # Add interactive bash to support `poetry shell`
+            pkgs.bashInteractive
+          ];
         in
         {
-          packages = {
-            inherit jupyterlab;
-            git-lfs = nixpkgs.legacyPackages.${system}.git-lfs; # Add git-lfs here
-          };
-          packages.default = jupyterlab;
-          apps.default.program = "${jupyterlab}/bin/jupyter-lab";
-          apps.default.type = "app";
-          # kernel.python.aiml.enable = true;
+          # Default shell with only poetry installed
+          devShells = {
+            default = pkgs.mkShell {
+              name = "default";
+              buildInputs = shellBuildInputs;
+              shellHook = ''
+                export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+                  pkgs.stdenv.cc.cc
+                ]}
+              '';
+            };
 
-          # Add a shell with python and pip:
-          devShell = pkgs.mkShell {
-            buildInputs = [
-              # Offer python, pip and poetry 
-              # for people who want to install additional packages
-              # using there own way
-              pkgs.python3
-              pkgs.poetry
-              pkgs.python3Packages.pip
+            # Install and load a poetry shell
+            poetry = pkgs.mkShell {
+              buildInputs = shellBuildInputs;
+              shellHook = ''
+                export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+                  pkgs.stdenv.cc.cc
+                ]}
+                poetry install
+                poetry shell
+              '';
+            };
 
-              # Add jupyter notebook command line tool
-              pkgs.jupyter
+            # Run jupyter lab in a poetry shell
+            lab = pkgs.mkShell {
+              buildInputs = shellBuildInputs;
+              shellHook = ''
+                export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+                  pkgs.stdenv.cc.cc
+                ]}
+                poetry install
+                poetry run jupyter-lab
+              '';
+            };
 
-              # Add interactive bash to support `poetry shell`
-              pkgs.bashInteractive
-
-            ];
-            shellHook = ''
-              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
-                pkgs.stdenv.cc.cc
-              ]}
-            '';
           };
         }
       );
